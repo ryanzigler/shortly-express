56b3ca6628e838c92c81615a10104ef113df02d4
Ryan Zigler <rpzigler@gmail.com>
2020-07-24 22:38:27 -0500

    Started implementing cookieParser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,3 +8,20 @@ module.exports.createSession = (req, res, next) => {
 // Add additional authentication middleware functions below
 /************************************************************/
 
+/*
+// if no cookies, generate new session with unique hash and store in sessions database
+    // use hash to set a cookie in res.headers
+
+  if (req.cookies. === undefined)
+
+  // if cookie exists, check that it's valid in session database
+
+  // if cookie exists and it's not valid, replace invalid cookie with new cookie and create new session
+
+  Object.keys
+  req.cookies = {}
+
+  req.cookies =
+
+  session.Sessions.get({});
+*/
