13f04816858566cf53c170e4ed4d0954a2e60bd3
Ryan Zigler <rpzigler@gmail.com>
2020-07-24 19:38:55 -0500

    Implemented user signup and finishing completing user login

--- server/app.js
+++ server/app.js
@@ -17,62 +17,116 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 
 
-app.get('/', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/create', 
-(req, res) => {
-  res.render('index');
-});
+app.get('/create',
+  (req, res) => {
+    res.render('index');
+  });
 
-app.get('/links', 
-(req, res, next) => {
-  models.Links.getAll()
-    .then(links => {
-      res.status(200).send(links);
-    })
-    .error(error => {
-      res.status(500).send(error);
-    });
-});
-
-app.post('/links', 
-(req, res, next) => {
-  var url = req.body.url;
-  if (!models.Links.isValidUrl(url)) {
-    // send back a 404 if link is not valid
-    return res.sendStatus(404);
-  }
-
-  return models.Links.get({ url })
-    .then(link => {
-      if (link) {
-        throw link;
-      }
-      return models.Links.getUrlTitle(url);
-    })
-    .then(title => {
-      return models.Links.create({
-        url: url,
-        title: title,
-        baseUrl: req.headers.origin
+app.get('/links',
+  (req, res, next) => {
+    models.Links.getAll()
+      .then(links => {
+        res.status(200).send(links);
+      })
+      .error(error => {
+        res.status(500).send(error);
       });
-    })
-    .then(results => {
-      return models.Links.get({ id: results.insertId });
-    })
-    .then(link => {
-      throw link;
-    })
-    .error(error => {
-      res.status(500).send(error);
-    })
-    .catch(link => {
-      res.status(200).send(link);
+  });
+
+app.post('/links',
+  (req, res, next) => {
+    var url = req.body.url;
+    if (!models.Links.isValidUrl(url)) {
+      // send back a 404 if link is not valid
+      return res.sendStatus(404);
+    }
+
+    return models.Links.get({ url })
+      .then(link => {
+        if (link) {
+          throw link;
+        }
+        return models.Links.getUrlTitle(url);
+      })
+      .then(title => {
+        return models.Links.create({
+          url: url,
+          title: title,
+          baseUrl: req.headers.origin
+        });
+      })
+      .then(results => {
+        return models.Links.get({ id: results.insertId });
+      })
+      .then(link => {
+        throw link;
+      })
+      .error(error => {
+        res.status(500).send(error);
+      })
+      .catch(link => {
+        res.status(200).send(link);
+      });
+  });
+
+app.post('/signup', (req, res, next) => {
+
+  models.Users.get({username: req.body.username})
+    .then((userExists) => {
+      if (userExists === undefined) {
+        models.Users.create(req.body)
+          .then(() => {
+            res.redirect('/');
+          });
+      } else {
+        res.redirect('/signup');
+      }
     });
 });
+
+app.post('/login', (req, res, next) => {
+  // user goes to login page
+  // user tries to login
+  // check if username exists
+  // models.Users.get({username: user})
+  // GET username, password, and salt from database
+  // models.Users.get({username: user}, {password: req.body.password}, {salt: req.body.salt})
+  // compare attempted password, password on file, and salt
+  // models.Users.compare(attempted, password, salt)
+  // if passes, redirect to home
+  // res.redirect('/');
+  // if fails, keep on login page
+  // res.redirect('/login');
+
+  models.Users.get({username: user})
+    .then((userExists) => {
+      if (userExists !== undefined) {
+        models.Users.compare(req.body.password, userExists.password, userExists.salt)
+          .then((dataComparison) => {
+            if (dataComparison) {
+              res.redirect('/');
+            } else {
+              res.redirect('/login');
+            }
+          });
+      }
+    });
+});
+
+
+//  params: {},
+//     query: {},
+//     res: [Circular],
+//     body: { username: 'Samantha', password: 'Samantha' },
+//     _body: true,
+//     length: undefined,
+//     route: Route { path: '/signup', stack: [Array], methods: [Object] },
+//     [Symbol(kCapture)]: false
 
 /************************************************************/
 // Write your authentication routes here
